15e6006f32c053fc0c3f034b8655250829404276
Colin Wong <cwongmath@gmail.com>
2023-09-29 04:05:23 -0500

    load json

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import json
 import requests
 
 API_KEY = os.environ.get('API_KEY')
@@ -10,9 +11,13 @@ r = requests.get('https://api.utdnebula.com/section', headers={
     'x-api-key': API_KEY,
     'Accept': 'application/json',
 }, params={
+    'academic_session.name': '23F',
     'meetings.location.building': 'SCI',
     # 'instruction_mode': 'hybrid',
 })
 
 print(r.status_code)
-print(r.content)
+
+obj = json.loads(r.text)
+
+print(len(obj['data']))
