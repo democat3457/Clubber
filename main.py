dc2e344cfc108c05bbd7b644b988eb853ceb1626
Colin Wong <cwongmath@gmail.com>
2023-10-16 02:00:58 -0500

    Push into main method

--- main.py
+++ main.py
@@ -7,17 +7,32 @@ import requests
 
 API_KEY = os.environ.get('API_KEY')
 
-r = requests.get('https://api.utdnebula.com/section', headers={
-    'x-api-key': API_KEY,
-    'Accept': 'application/json',
-}, params={
-    'academic_session.name': '23F',
-    'meetings.location.building': 'SCI',
-    # 'instruction_mode': 'hybrid',
-})
+def make_request(path: str, params: dict = {}):
+    return requests.get(f'https://api.utdnebula.com/{path}', headers={
+        'x-api-key': API_KEY,
+        'Accept': 'application/json',
+    }, params=params)
 
-print(r.status_code)
+def find_by_id(type: str, id: str):
+    return make_request(f'{type}/{id}')
 
-obj = json.loads(r.text)
+def main():
+    r = make_request('section', params={
+        'academic_session.name': '23F',
+        'meetings.location.building': 'SLC',
+        'meetings.location.room': '1.102',
+        'meetings.meeting_days': 'Monday',
+        'meetings.start_time': '0000-01-01T10:00:00-05:50',
+        # 'instruction_mode': 'hybrid',
+    })
 
-print(len(obj['data']))
+    print(r.status_code)
+
+    obj = json.loads(r.text)
+
+    print(obj['data'][0])
+
+    print(len(obj['data']))
+
+if __name__ == '__main__':
+    main()
